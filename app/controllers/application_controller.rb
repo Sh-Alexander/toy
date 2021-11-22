be3e0276834b4c3b098e70e675952d92b1016a14
Sh-Alexander <Fafa.89@mail.ru>
2021-11-22 21:04:36 +0300

    new name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     def hello
-    render plain: "привет задротам"
+    render plain: "Детейлинг в России"
     end 
 end
