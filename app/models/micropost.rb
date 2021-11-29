92a037ce47a58883db36cdd159266c6aa74546c7
Sh-Alexander <Fafa.89@mail.ru>
2021-11-29 21:49:34 +0300

    test massege

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
-class Micropost < ActiveRecord::Base
+class Micropost < ApplicationRecord
     belongs_to :user
-    validates :content, length: { maximum: 140}
+    validates :content, length: { maximum: 140},
+            presens: true 
 end
